654a86cad67331c70208e4d03f5a2a4ae758b09a
DeePenguin <alivemushroom@gmail.com>
2023-04-24 23:38:57 +0400

    feat: add footer

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -1,9 +1,13 @@
-import './index.scss';
+import './app.scss';
 
 import header from '../header';
+import main from '../main';
+import footer from '../footer';
 
 export default class App {
   constructor() {
     this.header = header;
+    this.main = main;
+    this.footer = footer;
   }
 }
